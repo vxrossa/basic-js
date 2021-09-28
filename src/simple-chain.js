2e40f2d79ea0bc204559378b8e8e4c3625340319
vxrossa <ibasalaev@icloud.com>
2021-09-28 14:00:21 +0300

    solve simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,6 +4,39 @@ import { NotImplementedError } from '../extensions/index.js';
  * Implement chainMaker object according to task description
  * 
  */
-export default {
-
+ export default {
+  chainLength: 0,
+  linkString: '',
+  chainArray: [],
+  getLength() {
+    return this.chainLength;
+  },
+  addLink (value) {
+    this.chainLength += 1;
+    this.chainArray.push(value);
+    return this;
+  },
+  removeLink (position) {
+    if(position > this.chainArray.length || isNaN(position) || position < 1 || typeof(position) != "number"){
+      this.chainArray = [];
+      throw new Error('You can\'t remove incorrect link!');
+    }
+    this.chainArray.splice(position - 1, 1);
+    this.chainLength -= 1;
+    return this;
+  },
+  reverseChain () {
+    this.chainArray.reverse();
+    return this;
+  },
+  finishChain () {
+    for(let i = 0; i < this.chainArray.length; i+= 1){
+      if(i >= 1){
+        this.linkString = this.linkString.concat('~~');
+      };
+      this.linkString = this.linkString.concat(`( ${this.chainArray[i]} )`);
+    };
+    this.chainArray = [];
+    return this.linkString;
+  }
 };
