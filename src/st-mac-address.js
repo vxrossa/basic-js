32f3d0209ed36e47f11c19b1ed6c603e3b6b6556
vxrossa <ibasalaev@icloud.com>
2021-09-28 16:05:01 +0300

    feat: solve mac address task

--- src/st-mac-address.js
+++ src/st-mac-address.js
@@ -14,7 +14,7 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-export default function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function isMAC48Address(n) {
+  let regularExpression = /^([0-9A-F]{2}\-){5}[0-9A-F]{2}$/
+  return regularExpression.test(n);
 }
