940cce122d04b3fd985d2497f7d4c9e3e432d126
vxrossa <ibasalaev@icloud.com>
2021-09-29 17:31:40 +0300

    feat: solve cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,75 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type){
+    type === false ? this.type = 'reverse' : this.type = 'direct';
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(phrase,key) {
+    if(!phrase || !key || typeof(phrase) == undefined){
+      throw new Error('Incorrect arguments!');
+    }
+
+    let alSpl = this.alphabet.split('');
+    let phraseSplit = phrase.toUpperCase().split('');
+    let keyExtended = key.repeat(Math.ceil(phrase.length / key.length)).substr(0,phrase.length).toUpperCase().split('');
+    let phraseEncrypt = [];
+
+    for(let i = 0; i < phraseSplit.length; i++){
+      if(phraseSplit[i] == ' '){
+        keyExtended.splice(i,0,' ')
+      }
+    }
+
+    for(let i = 0; i < phraseSplit.length; i++){
+      if(alSpl.indexOf(phraseSplit[i]) == -1){
+        phraseEncrypt.push(phraseSplit[i]);
+      } else {
+        if(alSpl.indexOf(phraseSplit[i]) + alSpl.indexOf(keyExtended[i]) < 26){
+          phraseEncrypt.push(alSpl[alSpl.indexOf(phraseSplit[i]) + alSpl.indexOf(keyExtended[i])]);
+        } else if (alSpl.indexOf(phraseSplit[i]) + alSpl.indexOf(keyExtended[i]) >= 26) {
+          phraseEncrypt.push(alSpl[alSpl.indexOf(phraseSplit[i]) + alSpl.indexOf(keyExtended[i]) - 26]);
+        }
+      }
+      
+    }
+    if(this.type == 'direct'){
+      return phraseEncrypt.join('');
+    } else {
+      return phraseEncrypt.reverse().join('');
+    }
+  }
+  decrypt(phrase,key) {
+    if(!phrase || !key || typeof(phrase) == undefined){
+      throw new Error('Incorrect arguments!');
+    }
+
+    let alSpl = this.alphabet.split('');
+    let phraseSplit = phrase.toUpperCase().split('');
+    let keyExtended = key.repeat(Math.ceil(phrase.length / key.length)).substr(0,phrase.length).toUpperCase().split('');
+    let phraseDecrypt = [];
+
+    for(let i = 0; i < phraseSplit.length; i ++){
+      if(phraseSplit[i] == ' '){
+        keyExtended.splice(i,0,' ');
+      }
+    }
+  
+    for(let i = 0; i < phraseSplit.length; i++){
+      if(alSpl.indexOf(phraseSplit[i]) == -1){
+        phraseDecrypt.push(phraseSplit[i]);
+      } else {
+        if(alSpl.indexOf(phraseSplit[i]) - alSpl.indexOf(keyExtended[i]) >= 0){
+          phraseDecrypt.push(alSpl[alSpl.indexOf(phraseSplit[i]) - alSpl.indexOf(keyExtended[i])]);
+        } else if (alSpl.indexOf(phraseSplit[i]) - alSpl.indexOf(keyExtended[i]) < 0){
+          phraseDecrypt.push(alSpl[26 + (alSpl.indexOf(phraseSplit[i]) - alSpl.indexOf(keyExtended[i]))]);
+        }
+      }
+    }
+    if(this.type == 'direct'){
+      return phraseDecrypt.join('');
+    } else {
+      return phraseDecrypt.reverse().join('');
+    }
   }
 }
