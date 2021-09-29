83160152ea7cee1d639835e308c8bbafd6f11ac7
vxrossa <ibasalaev@icloud.com>
2021-09-29 20:30:26 +0300

    feat: solve extended repeater with minor errors

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,34 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  let repetition = options.repeatTimes;
+  let stringSeparator = options.separator; // +
+  let separatorAddition = options.addition;
+  let addRepetition = options.additionRepeatTimes;
+  let repSeparator = options.additionSeparator; // |
+
+  if(options.separator == undefined){
+    stringSeparator = '+';
+  }
+  if(options.additionSeparator == undefined){
+    repSeparator = '|';
+  }
+  if(options.addition == undefined){
+    separatorAddition = '';
+  }
+  if(options.additionRepeatTimes == undefined){
+    addRepetition = 1;
+  }
+
+  let repeated = '';
+  let completeSeparator = '';
+
+  completeSeparator = completeSeparator.concat((separatorAddition + repSeparator).repeat(addRepetition - 1) + separatorAddition);
+  // console.log(`Complete separator is : ${completeSeparator}`);
+
+  repeated = repeated.concat((str.toString() + completeSeparator + stringSeparator).repeat(repetition - 1) + (str.toString() + completeSeparator));
+  // console.log(`Repeated stuff is ${repeated}`);
+
+  return repeated;
 }
