04288f91a1fbdc5313b6072bb57f62e0cea72a9b
vxrossa <ibasalaev@icloud.com>
2021-10-04 18:52:42 +0300

    feat: solve encode line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,29 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let result = [];
+  let array = [];
+  let count = [];
+  let countNum = 1;
+  let strSplit = str.split('');
+  for(let i = 0; i < strSplit.length; i++){
+    if(strSplit[i+1] === strSplit[i]){
+      countNum += 1;
+    }
+    if(strSplit[i+1] !== strSplit[i]){
+      array.push(strSplit[i]);
+      count.push(countNum);
+      countNum = 1;
+    }
+  }
+  for(let i = 0; i < array.length; i++){
+    if(count[i] === 1){
+      result.push(`${array[i]}`);
+    }
+    if(count[i] > 1){
+      result.push(`${count[i]}${array[i]}`);
+    }
+  }
+  return result.join('');
 }
